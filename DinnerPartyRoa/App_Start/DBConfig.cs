9058dc592e1d8d3a0e224aa10810a6abfc0954c5
user <user>
2015-06-15 11:38:59 +1200

    setup auto db migrations on app start

---
+++ DinnerPartyRoa/App_Start/DBConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using DinnerPartyRoa.Models;
+using DinnerPartyRoa.Migrations;
+
+namespace DinnerPartyRoa.App_Start
+{
+    public  static class DBConfig
+    {
+        public static void RunDbMigrations()
+        {
+            Database.SetInitializer<ApplicationDbContext>(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+    }
+}
--- DinnerPartyRoa/Global.asax.cs
+++ DinnerPartyRoa/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DinnerPartyRoa.App_Start;
 
 namespace DinnerPartyRoa
 {
@@ -13,11 +14,15 @@ namespace DinnerPartyRoa
     {
         protected void Application_Start()
         {
+
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DBConfig.RunDbMigrations();
         }
     }
 }
