1fcdb98a2f4de9bd443f33a7be49d9a3bcae0c76
user <user@gmail.com>
2015-06-17 17:31:52 +1200

    Read By Quantity

--- DinnerPartyRoa/Models/OrderData.cs
+++ DinnerPartyRoa/Models/OrderData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Web;
+using Microsoft.Owin.Security.Provider;
 
 namespace DinnerPartyRoa.Models
 {
@@ -10,15 +11,23 @@ namespace DinnerPartyRoa.Models
     {
         ApplicationDbContext db = new ApplicationDbContext();
 
-        //Re-Arrange method to render ViewModel for view
-
         public List<Order> Read()
         {
             Order newestOrder = db.Orders.OrderByDescending(n => n.CreatedOn).FirstOrDefault();
             DateTime weeksOrder = newestOrder.CreatedOn.Date;
-            List<Order> orders = db.Orders.Where(s => EntityFunctions.TruncateTime(s.CreatedOn) == weeksOrder).ToList();
+            List<Order> orders = db.Orders.Where(s => EntityFunctions.TruncateTime(s.CreatedOn) >= weeksOrder &&  < weeksOrder).ToList();
             return orders;
         }
-        
+
+        public Dictionary<MenuItem, int> ReadByQuantity()
+        {
+            List<Order> data = Read();
+            Dictionary<MenuItem, int> orderSummary = new Dictionary<MenuItem, int>();
+            foreach (var i in data)
+            {
+                orderSummary.Add();
+            }
+            return orderSummary;
+        }   
     }
 }
