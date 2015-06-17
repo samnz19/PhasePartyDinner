c0d7ed28df42fb75d04d222fd00383a9d296c152
user <user>
2015-06-17 15:26:42 +1200

    admin page layout done

---
+++ DinnerPartyRoa/Models/ScrapperMenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DinnerPartyRoa.Models
+{
+    public class ScrapperMenuViewModel
+    {
+        public List<MenuItem> ArroyMenu { get; set; }
+        public List<MenuItem> DatabaseMenu { get; set; }
+
+     
+    }
+}
