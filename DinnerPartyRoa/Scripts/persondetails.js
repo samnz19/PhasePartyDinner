439efa5840ff78c3853ee1b8ed886a62013fe685
user <user@users.noreply.github.com>
2015-06-18 13:30:16 +1200

    formated personaldetails.js

--- DinnerPartyRoa/Scripts/persondetails.js
+++ DinnerPartyRoa/Scripts/persondetails.js
@@ -1,19 +1,15 @@
 ﻿$(document).ready(function () {
-   // alert('hi peepz');
+
     var array = [];
     var currentSelectedItem = "";
 
-    $('#col1').on('click', 'li', function() {
+    $('#col1').on('click', 'li', function () {
         $('#currentorder').empty();
         currentSelectedItem = $.trim($(this).text());
-        console.log(currentSelectedItem);
-        //var div = $("div")[0];
-        //var orderId = $(this).data(div, "left");
+
         var orderDiv = $(this).find("div")[0];
         var orderId = $(orderDiv).data("menuitemid");
-        console.log(this);
-        console.log(orderDiv);
-        console.log(orderId);
+
         $('#currentorderid').attr("value", orderId);
         $('#currentorder').attr("value", currentSelectedItem);
         $('#currentorder').css('color', 'blue');
@@ -24,42 +20,33 @@
         source: array
     });
 
-       $('#submitbutton').on('click', function(e) {
+    $('#submitbutton').on('click', function (e) {
         e.preventDefault();
         var order = new Order();
         order.Item = $('#currentorder').val();
         order.ItemId = $('#currentorderid').val();
-        //order.ItemId = orderId;
+
         order.User = $('#currentuser').val();
 
         $.ajax({
             type: 'POST',
             url: '/api/orders/',
             data: order,
-            //data: {
-            //    user: {name: order.User},
-            //    item: { title: order.Item }
-            //},
-            datatype: 'json',
-            success: function() {
 
+            datatype: 'json',
+            success: function () {
 
             }
         });
 
-      });
-   
+    });
+
 });
-
-
-
 
 function Order() {
     User = null;
     Item = null;
 }
-
-
 
 var GetNames = function () {
 
@@ -68,19 +55,15 @@ var GetNames = function () {
         type: "GET",
         url: "https://api.github.com/orgs/enspiral-dev-academy/members",
         success: function (data) {
-           
+
             for (var key in data) {
-               
                 array.push(data[key].login);
             }
         },
         error: function () {
-            //$("#CustomerDetailsList").empty();
-            //$("#CustomerDetailsList").append("<li>Error getting data from server...</li>");
             alert("wrong");
         }
     });
-
     return array;
 }
 
