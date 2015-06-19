ece2e05f8a628503166aae907d32a3238207fbf8
Max Lott <maxlott8@gmail.com>
2015-06-19 22:25:19 +1200

    Auto complete fixed (badly) full name does go through

--- DinnerPartyRoa/Scripts/persondetails.js
+++ DinnerPartyRoa/Scripts/persondetails.js
@@ -16,55 +16,21 @@
         $('#currentorder').css('color', 'black');
     });
 
-   
+
     array = GetNames();
-    $("#currentuser").autocomplete({
-        source: array,
-        select: function (event, ui) {
-            $("#currentuser").val(ui.item.value);
-        }
 
+    $(function () {
+        var userList = $("#currentuser").autocomplete({
+            source: array,
+            change: function(event, ui) {
+                var val = ui.item.value;
+                $('#currentuser').attr('value', val)
+                //$('#currentuser').val(val)
+                          },
+           });
     });
    
-    
-    $('#submitbutton').on('click', function (e) {
-        $("#currentuser").val()
-        console.log($("#currentuser").val())
-    });
-
-    //$('#menu').on('click', 'li', function () {
-    //    $('#currentorder').empty();
-    //    currentSelectedItem = $.trim($(this).text());
-
-    //    var orderDiv = $(this).find("#data")[0];
-    //    var orderId = $(orderDiv).data("menuitemid");
-
-    //    $('#currentorderid').attr("value", orderId);
-    //    $('#currentorder').attr("value", currentSelectedItem);
-    //    $('#currentorder').css('color', 'blue');
-    //});
-
-
-    //$('#submitbutton').on('click', function (e) {
-    //    e.preventDefault();
-    //    var order = new Order();
-    //    order.Item = $('#currentorder').val();
-    //   order.ItemId = $('#currentorderid').val();
-
-    //    order.User = $('#currentuser').val();
-
-    //    $.ajax({
-    //        type: 'POST',
-    //        url: '/api/orders/',
-    //        data: order,
-
-    //        datatype: 'json',
-    //        success: function () {
-    //            alert("Order Placed!");
-    //        }
-    //    });
-
-    //});
+ 
 
  });
 
@@ -97,7 +63,6 @@ var GetNames = function () {
 
 
 //AngularJs
-
 var menuApp = angular.module('menuApp', []);
 
 menuApp.controller('ListCtrl', function ($scope, $http) {
@@ -107,18 +72,31 @@ menuApp.controller('ListCtrl', function ($scope, $http) {
         $scope.user = "";
 
         $scope.meals = dataAng;
+        console.log(dataAng)
+
         $scope.menulist = function (meal) {
 
             $scope.meal.Title = meal.Title;
             $scope.meal.Id = meal.Id;
+            console.log(meal.Title)            
+        }
+
+        $scope.test = function () {
+
+            console.log($scope.user)      // problem being that doesnt registor jquery change function as a change to scope        
+           
         }
 
         $scope.submit = function () {
 
+            console.log($scope.user)              //uhmmmmmmmmmmm....nice autoupdate...
+            console.log($('#currentuser').val()) //isnt this the same?
+
+
             var order = new Order();
             order.Item = $scope.meal.Title;
             order.ItemId = $scope.meal.Id;
-            order.User = $scope.user;
+            order.User = $('#currentuser').val();  //total legit..............
 
             $http.post('/api/orders/', order).
                 success(function () {
@@ -129,11 +107,10 @@ menuApp.controller('ListCtrl', function ($scope, $http) {
 
                 });
         }
-    }); 
+    });
 });
 
 
-    
 
 
 
@@ -142,3 +119,36 @@ menuApp.controller('ListCtrl', function ($scope, $http) {
 
 
 
+//$('#menu').on('click', 'li', function () {
+//    $('#currentorder').empty();
+//    currentSelectedItem = $.trim($(this).text());
+
+//    var orderDiv = $(this).find("#data")[0];
+//    var orderId = $(orderDiv).data("menuitemid");
+
+//    $('#currentorderid').attr("value", orderId);
+//    $('#currentorder').attr("value", currentSelectedItem);
+//    $('#currentorder').css('color', 'blue');
+//});
+
+
+//$('#submitbutton').on('click', function (e) {
+//    e.preventDefault();
+//    var order = new Order();
+//    order.Item = $('#currentorder').val();
+//   order.ItemId = $('#currentorderid').val();
+
+//    order.User = $('#currentuser').val();
+
+//    $.ajax({
+//        type: 'POST',
+//        url: '/api/orders/',
+//        data: order,
+
+//        datatype: 'json',
+//        success: function () {
+//            alert("Order Placed!");
+//        }
+//    });
+
+//});
