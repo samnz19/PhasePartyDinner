f999d12f03b77db64ce4ae3459ae3f274ce82ff9
user <user>
2015-06-19 12:18:16 +1200

    deleted edit button

--- DinnerPartyRoa/Controllers/OrderDetailsController.cs
+++ DinnerPartyRoa/Controllers/OrderDetailsController.cs
@@ -81,7 +81,7 @@ namespace DinnerPartyRoa.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Username,CreatedOn")] Order order)
+        public ActionResult Edit([Bind(Include = "Id,Username,Title")] Order order)
         {
             if (ModelState.IsValid)
             {
